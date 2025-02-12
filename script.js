ccc908928bfe497f14876760a49dfb123e4ee333
Rune <runematthews@gmail.com>
2025-02-11 22:25:32 -0800

    Update script.js
    Trying to script send

---
+++ script.js
@@ -0,0 +1,5 @@
+<script type="text/javascript">
+  function sendEmail() {
+    Email.send({ Host: "https://app.getform.io/forms/broylema", To: 'https://getform.io/f/broylema' })
+      .then(function (message) { alert("Mail sent successfully"); }); }
+  </script>
